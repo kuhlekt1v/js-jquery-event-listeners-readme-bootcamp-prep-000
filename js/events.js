d35cb7db4f7c36887523552d8c706fba5f9fe538
Cody Sheridan <csheridan994@gmail.com>
2018-05-09 20:46:13 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!");
+    return;
+  });
+}
 
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty');
+    return;
+  });
+}
+  
+  
 $(document).ready(function(){
 
 // call functions here
+
+//Hey! Popup when clicked
+$("p").on("click", function() {
+    alert("Hey!");
+    return;
+  });
+  
+//Adds red frame to image
+$('img').on('load', function() {
+    $('img').addClass('tasty');
+    return;
+  });
 
 });
