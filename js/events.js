662a5803d90deb78feff55180d683681309cea65
Cody Sheridan <csheridan994@gmail.com>
2018-05-09 21:02:47 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -2,14 +2,12 @@
 function getIt() {
   $('p').on('click', function() {
     alert("Hey!");
-    return;
   });
 }
 
 function frameIt() {
   $('img').on('load', function() {
     $('img').addClass('tasty');
-    return;
   });
 }
 
